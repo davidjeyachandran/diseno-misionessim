800c9da6651b8d51d59c5a16e72ddbf994256d49
David Jeyachandran <davidjeyachandran@gmail.com>
2017-06-27 21:18:13 -0500

    Add missing images and correct css

--- js/parallax.init.js
+++ js/parallax.init.js
@@ -6,14 +6,14 @@
 		selector.addClass('parallax_section');
 		selector.attr('data-type-media', 'image');
 		selector.wrapInner('<div class="container parallax_content"></div>');
-		selector.append('<div class="parallax_inner"><img class="parallax_media" src="/themes/'+path+filename+'" data-base-width="'+width+'" data-base-height="'+height+'"/></div>');
+		selector.append('<div class="parallax_inner"><img class="parallax_media" src="'+path+filename+'" data-base-width="'+width+'" data-base-height="'+height+'"/></div>');
 
 		selector.tmMediaParallax();
 	}
 
 	$(window).load(function(){
-		addImageParallax('#tmhtmlcontent_topColumn','bon_voyage/images/','img-1.jpg','2050','532');
-		addImageParallax('#tmhtmlcontent_home','bon_voyage/images/','img-2.jpg','2050','683');
+		addImageParallax('#tmhtmlcontent_topColumn','images/','img-1.jpg','2050','532');
+		addImageParallax('#tmhtmlcontent_home','images/','img-2.jpg','2050','683');
 
 	});
 })(jQuery);
