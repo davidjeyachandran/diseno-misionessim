324a1c9cb1db0977bb35f5b276ad42fb85cba5cc
David Jeyachandran <davidjeyachandran@gmail.com>
2017-07-03 17:33:58 -0500

    Add js for 4 column animation for revista vamos

--- js/view-1.init.js
+++ js/view-1.init.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function(){
 	jQuery("#block-views-block-view-1-block-1 .views-view-grid > .row > .col-sm-4 .views-field-title a").wrapInner("<div><h2></h2></div>");
 	jQuery("#block-views-block-view-1-block-1 .views-view-grid > .row > .col-sm-4 .views-field-title a > div").addClass("item-html");
+
+  jQuery("#block-views-block-view-1-block-1 .views-view-grid > .row > .col-sm-3 .views-field-title a").wrapInner("<div><h2></h2></div>");
+  jQuery("#block-views-block-view-1-block-1 .views-view-grid > .row > .col-sm-3 .views-field-title a > div").addClass("item-html");
+
 })
 
 jQuery(window).scroll(function() {
